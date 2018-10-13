090a6506d3343b8a88c29ef448e74f9bf5e1354d
PauliusKu <paulius.kuprevicius@gmail.com>
2018-10-13 11:23:00 +0300

    First try

--- Redis_1/LogIn.Designer.cs
+++ Redis_1/LogIn.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Redis_1
 {
-    partial class Form1
+    partial class LogIn
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ButLogIn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // ButLogIn
+            // 
+            this.ButLogIn.AccessibleName = "LogIn";
+            this.ButLogIn.Location = new System.Drawing.Point(219, 225);
+            this.ButLogIn.Name = "ButLogIn";
+            this.ButLogIn.Size = new System.Drawing.Size(334, 64);
+            this.ButLogIn.TabIndex = 0;
+            this.ButLogIn.Text = "LogIn";
+            this.ButLogIn.UseVisualStyleBackColor = true;
+            this.ButLogIn.Click += new System.EventHandler(this.ButLogIn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(219, 169);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // LogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 521);
-            this.Name = "Form1";
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ButLogIn);
+            this.Name = "LogIn";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButLogIn;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
---
+++ Redis_1/LogIn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StackExchange.Redis;
+
+namespace Redis_1
+{
+    public partial class LogIn : Form
+    {
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void ButLogIn_Click(object sender, EventArgs e)
+        {
+            IDatabase db = redis.GetDatabase();
+            Random rnd = new Random();
+            int value = rnd.Next(1, 100);
+            db.StringSet("mykey", value);
+            string retrievevalue = db.StringGet("mykey");
+            textBox1.Text = retrievevalue;
+        }
+    }
+}
